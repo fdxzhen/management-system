ec2dfac32157f09fbe9334128238591eb65b7c4e
zhenhao <18738989958@qq.com>
2020-12-24 16:37:33 +0800

    Revert "111"
    This reverts commit 18c982e3

--- system-controller/src/main/java/com/zhenhao/controller/account/RbacMenuController.java
+++ system-controller/src/main/java/com/zhenhao/controller/account/RbacMenuController.java
@@ -9,6 +9,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RbacMenuController {
     public static void main(String[] args) {
         System.out.println(111);
-        System.out.println(222);
     }
 }
