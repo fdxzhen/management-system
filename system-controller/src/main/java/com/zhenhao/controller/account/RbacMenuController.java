18c982e3186ffd3d8c4639c32088da60bdbe57d9
zhenhao <18738989958@qq.com>
2020-12-24 16:34:54 +0800

    111

--- system-controller/src/main/java/com/zhenhao/controller/account/RbacMenuController.java
+++ system-controller/src/main/java/com/zhenhao/controller/account/RbacMenuController.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("system/menu")
 public class RbacMenuController {
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
 }
